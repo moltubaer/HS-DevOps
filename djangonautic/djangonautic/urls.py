5f6e5bfdac70d5d87beaca0e62731ffbec77bcd3
alexamol <alexamol@stud.ntnu.no>
2021-03-17 19:34:04 +0100

    static files & images

--- djangonautic/djangonautic/urls.py
+++ djangonautic/djangonautic/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
 from .import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +10,6 @@ urlpatterns = [
     url(r'^about/$', views.about),
     url(r'^$', views.homepage),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+
