11f1308ccd4e9d4282a2796cfe1693ce8e226368
alexamol <alexamol@stud.ntnu.no>
2021-03-17 18:44:09 +0100

    Template Tags

--- djangonautic/articles/models.py
+++ djangonautic/articles/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    # Add in thumbnail alter
+    # Add in author later
+
+    def __str__(self):
+        return self.title
+
+
+"""
+python manage.py makemigrations
+python manage.py migrate
+
+^ Må kjøre kommandoene over for hver gang man redigerer eller lager en model
+"""
