61a8b8b5ec08dfcb4f9e11aea50ae0ec4dcdeb9a
alexamol <alexamol@stud.ntnu.no>
2021-03-22 19:35:41 +0100

    Article Detail Template

--- djangonautic/articles/urls.py
+++ djangonautic/articles/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from .import views
 
 urlpatterns = [
-    url(r'^$', views.article_list),
+    url(r'^$', views.article_list, name="list"),
+    url(r'^(?P<slug>[\w-]+)/$',views.article_detail, name="detail"),
 ]
