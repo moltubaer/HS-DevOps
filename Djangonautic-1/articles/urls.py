abc1e89898042ea912d63c00951c26d7dfe423f1
moltubaer <79594287+moltubaer@users.noreply.github.com>
2021-06-23 17:32:34 +0200

    Delete Djangonautic-1 directory

--- Djangonautic-1/articles/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from .import views
-
-urlpatterns = [
-    url(r'^$', views.article_list, name="list"),
-    url(r'^(?P<slug>[\w-]+)/$',views.article_detail, name="detail"),
-]
